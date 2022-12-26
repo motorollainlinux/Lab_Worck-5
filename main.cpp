773a05fe9986f4cf0502da39853187ab21ed5f95
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2022-12-26 16:03:10 +0700

    ???

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <fstream>
 
-int Matrix[5][5];
+void UserEnter(int Numbers[], int N) {
+    for (int i = 0; i < N; i++) {
+        std::cin >> Numbers[i];
+    }
+}
 
-void FileToArray() {
-    std::ifstream file;
-    file.open("file.txt");
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            file >> Matrix[i][j];
+int Function(int N, int* Numbers) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (Numbers[i] == Numbers[j] && i != j) {
+                return i;
+            } 
+        }
+        if (Numbers[i] > N || Numbers[i] < 1) {
+            return i;
         }
     }
-    file.close();
-} 
-int Function() {
-   int Result = 0;
-    for (int i = 0; i < 4; i++) {
-        Result += Matrix[i][i+1];
-    }
-    Result /= 4.0;
-    return Result;
+    return -1;
 }
+
 int main() {
-    int Res;
-    FileToArray();
-    Res = Function();
-    std::cout << "result: " << Res << "\n"; 
+    static int N = 10;
+    int Numbers[N];
+    int Result;
+    UserEnter( Numbers, N);
+    Result = Function(N, Numbers);
+    if (Result == -1) {
+        std::cout << "Function result: " << Result << "\n";
+    } else {
+        std::cout << "Function result: " << Result << " " << Numbers[Result] << "\n";
+    }
     return 0;
 }
